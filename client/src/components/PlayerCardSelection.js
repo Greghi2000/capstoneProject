3656b88ff991b2db44786a49b0bcce43d2cfde10
Greghi2000 <greghi.fumagalli@gmail.com>
2023-06-13 14:41:32 +0100

    Added CSS to PlayerCardSelection

--- client/src/components/PlayerCardSelection.js
+++ client/src/components/PlayerCardSelection.js
@@ -1,83 +1,87 @@
 import React, { useState } from "react";
 import axios from 'axios';
-const PlayerCardSelection = ({activePlayer, setActivePlayer}) => {
-    const [activePlayerSelectedHand, setActivePlayerSelectedHand] = useState([])
-    const [activePlayerSelectedCard, setActivePlayerSelectedCard] = useState(null)
+import './PlayerCardSelection.css';
 
+const PlayerCardSelection = ({ activePlayer, setActivePlayer }) => {
+  const [activePlayerSelectedHand, setActivePlayerSelectedHand] = useState([]);
+  const [activePlayerSelectedCard, setActivePlayerSelectedCard] = useState(null);
 
-    //choosing hand from deck
+  // Choosing hand from deck
 
-    const handleClick = (card, name, power) => {
-        console.log(`Clicked on card - Name: ${name} | Power: ${power}`);
-        //!check needed to stop a card being selected twice!
-        if (activePlayerSelectedHand.length < 10) {
-            setActivePlayerSelectedHand((prevHand) => [...prevHand, card]);
+  const handleClick = (card, name, power) => {
+    console.log(`Clicked on card - Name: ${name} | Power: ${power}`);
+    const isCardSelected = activePlayerSelectedHand.some((selectedCard) => selectedCard.id === card.id);
+    if (!isCardSelected && activePlayerSelectedHand.length < 10) {
+      setActivePlayerSelectedHand((prevHand) => [...prevHand, card]);
+    }
+  };
+
+  const handleHandSubmit = async (e) => {
+    if (activePlayer.hand.length >= 1) {
+      e.preventDefault();
+      console.log("You've already submitted your hand");
+    } else {
+      setActivePlayerSelectedHand([]);
+      e.preventDefault();
+      try {
+        const response = await axios.post(
+          'http://localhost:8080/api/players/hand',
+          activePlayerSelectedHand
+        );
+        if (response.status === 200) {
+          try {
+            const response = await axios.get('http://localhost:8080/api/gamestate/togglePlayer');
+            console.log(response.data);
+            setActivePlayer(response.data);
+          } catch (error) {
+            console.error(error);
+          }
         }
-      };
-
-      const handleHandSubmit = async (e) => {
-        if(activePlayer.hand.length >= 1) {
-            e.preventDefault();
-            console.log("You've already submitted your hand")
-        } else {
-        setActivePlayerSelectedHand([])
-        e.preventDefault();
-        try {
-          const response = await axios.post(
-            'http://localhost:8080/api/players/hand',
-            activePlayerSelectedHand
-          );
-          if(response.status === 200){
-            try {
-                const response = await axios.get('http://localhost:8080/api/gamestate/togglePlayer');
-                // Handle the response if needed
-                console.log(response.data);
-                setActivePlayer(response.data);
-              } catch (error) {
-                console.error(error);
-              }
-        }
-        } catch (error) {
-          console.error(error);
-        }
-      }};
-
-      //choosing card from hand 
-
-      const handleChosenCardClick = (card, name, power) => {
-        console.log(`Clicked on card - Name: ${name} | Power: ${power}`);
-        setActivePlayerSelectedCard(card);
+      } catch (error) {
+        console.error(error);
       }
+    }
+  };
 
-      const handleChosenCardSubmission = async (e) => {
-        e.preventDefault();
+  // Choosing card from hand 
+
+  const handleChosenCardClick = (card, name, power) => {
+    console.log(`Clicked on card - Name: ${name} | Power: ${power}`);
+    setActivePlayerSelectedCard(card);
+  };
+
+  const handleChosenCardSubmission = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/api/gamestate/playCard',
+        activePlayerSelectedCard
+      );
+      if (response.status === 200) {
         try {
-          const response = await axios.post(
-            'http://localhost:8080/api/gamestate/playCard',
-            activePlayerSelectedCard
-          );
-          if(response.status === 200){
-            try {
-                const response = await axios.get('http://localhost:8080/api/gamestate/togglePlayer');
-                // Handle the response if needed
-                console.log(response.data);
-                setActivePlayer(response.data);
-              } catch (error) {
-                console.error(error);
-              }
-        }
+          const response = await axios.get('http://localhost:8080/api/gamestate/togglePlayer');
+          console.log(response.data);
+          setActivePlayer(response.data);
         } catch (error) {
           console.error(error);
         }
       }
-      
-    return (
-      <>
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="PlayerCardSelection">
       {activePlayer &&
         (activePlayer.hand.length >= 1 ? (
           activePlayer.hand.map((card) => (
-            <div>
-              <p onClick={() => handleChosenCardClick(card, card.name, card.power)}>
+            <div
+              key={card.id}
+              className={`card ${activePlayerSelectedCard && activePlayerSelectedCard.id === card.id ? 'active' : ''}`}
+              onClick={() => handleChosenCardClick(card, card.name, card.power)}
+            >
+              <p>
                 Name of Card: {card.name} || Power: {card.power}
               </p>
             </div>
@@ -85,8 +89,12 @@ const PlayerCardSelection = ({activePlayer, setActivePlayer}) => {
         ) : (
           <>
             {activePlayer.deck.map((card) => (
-              <div key={card.id}>
-                <p onClick={() => handleClick(card, card.name, card.power)}>
+              <div
+                key={card.id}
+                className={`card ${activePlayerSelectedHand.some((selectedCard) => selectedCard.id === card.id) ? 'selected' : ''}`}
+                onClick={() => handleClick(card, card.name, card.power)}
+              >
+                <p>
                   Name of Card: {card.name} || Power: {card.power}
                 </p>
               </div>
@@ -100,10 +108,9 @@ const PlayerCardSelection = ({activePlayer, setActivePlayer}) => {
         <form onSubmit={handleChosenCardSubmission}>
           <input type="submit" value="Play Chosen Card" />
         </form>
-        
       )}
-    </>
-    );
-}
- 
+    </div>
+  );
+};
+
 export default PlayerCardSelection;
